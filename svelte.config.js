37889f9c8ea6315494ea13160aae641f4cbe3be4
adityaoberai <adityaoberai1@gmail.com>
2024-10-26 22:17:59 +0530

    fix sveltekit path

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter()
 	}
 };
 
