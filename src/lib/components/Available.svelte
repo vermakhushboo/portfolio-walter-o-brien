20aa892038973d344b3f4fa876392ad6d11ccdf8
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-10-27 10:44:13 +0100

    Update Available.svelte

--- src/lib/components/Available.svelte
+++ src/lib/components/Available.svelte
@@ -6,7 +6,7 @@
 <section id="available" class="container">
     <Typography.Title size="medium">
         <span>●</span>
-        Available for new projects
+        Available for new projects now
     </Typography.Title>
 
     <Typography.Text variant="l-500">Available for new partnerships and collaborations. Let's chat!</Typography.Text>
